a205da6fe0bceec5728cfbab278bb935dd8accd7
Doug Pfeil <dpfeil@gmail.com>
2020-04-03 11:07:54 -0400

    Add basic files to start using hooks

---
+++ api/CalendarAPI.js
@@ -0,0 +1,15 @@
+
+
+const CalendarAPI = {};
+
+
+CalendarAPI["getBlah"] = () => {
+    return "Blurgity blurg";
+}
+
+export { CalendarAPI }
+
+
+// client id: 1030460760568-8207d1iitbtq1mqentokp4j921j8umdo.apps.googleusercontent.com
+// client secret: MQgr8QxTKrRldaLGSIxRAxUa
+// api key: AIzaSyAT7Ue3zsaE8UW19RHRu3iiv04ia_ym7to
