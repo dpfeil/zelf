d821390c84a2439b9451abd823ca85caff323681
Doug Pfeil <dpfeil@gmail.com>
2020-05-22 19:20:20 -0400

    Add firebase config file

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: 'AIzaSyBE-dXQ-asoYAfZh7Tkb8eAZNDH3TAeM8c',
+  authDomain: 'zelf-2020.firebaseapp.com',
+  databaseURL: 'https://zelf-2020.firebaseio.com',
+  projectId: 'zelf-2020',
+  storageBucket: 'zelf-2020.appspot.com',
+  messagingSenderId: '156410866470',
+  appId: '1:156410866470:web:aa04e9bfd1265aafac6fe7',
+  measurementId: 'G-DRZW73L1BQ',
+};
+
+export default config;
