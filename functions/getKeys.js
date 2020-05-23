41fa82be01251497460cfd6169a4fc3b6b6c6e12
Doug Pfeil <dpfeil@gmail.com>
2020-05-23 12:20:37 -0400

    Add JSON stringify to body of getKeys

--- functions/getKeys.js
+++ functions/getKeys.js
@@ -1,9 +1,9 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       clientId: process.env.clientId,
       apiKey: process.env.apiKey,
-    },
+    }),
   });
 };
