d6f9495244a7febb8c80548b1f27fb18f6c60a2e
Doug Pfeil <dpfeil@gmail.com>
2020-06-03 16:32:21 -0400

    Clean up Calendar module

--- modules/Calendar.js
+++ modules/Calendar.js
@@ -1,34 +1,25 @@
 import { Button } from '@blueprintjs/core';
+import { useState } from 'react';
 
 const listUpcomingEvents =
   typeof window === 'undefined'
     ? require('../api/FakeFunction')
     : require('../api/AuthAPI').listUpcomingEvents;
 
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  const pre = document.getElementById('content');
-  const textContent = document.createTextNode(`${message}\n`);
-  pre.appendChild(textContent);
-}
-
-const printItems = () => {
-  listUpcomingEvents().then((result) => {
-    appendPre(result);
-    console.log(result);
-  });
-};
-
 const Calendar = () => {
+  const [items, setItems] = useState([]);
+
+  const getItems = () => {
+    listUpcomingEvents().then((result) => {
+      setItems(result);
+    });
+  };
+
   return (
     <div>
-      <Button text="get events" onClick={printItems} />
-      <pre id="content" style={{ whiteSpace: 'pre-wrap' }} />
+      <h2>Google Calendar</h2>
+      <Button text="get events" onClick={getItems} />
+      <div>{items}</div>
     </div>
   );
 };
